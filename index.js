21ce78e1f19d2f1515c564ac8bec2f307df954bc
Brian Tam <brian@imarc.com>
2018-07-31 14:52:36 -0700

    added ready state triggers and camouflage package

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // Require relevant packages
 var conditioner = require("offhand-conditioner");
+var camouflage  = require("offhand-camouflage");
+
+var completeState = null;
 
 // Bootstrap with default settings
 var bootstrap = function() {
-    conditioner('body');
+    if (completeState != document.readyState) {
+        switch (document.readyState) {
+            case 'interactive':
+                camouflage();
+                break;
+
+            case 'complete':
+                conditioner('body');
+                break;
+        }
+        
+        completeState = document.readyState;
+    }
 }
 
-if (document.readyState ==='complete') {
-    bootstrap();
-} else {
-    document.addEventListener('DOMContentLoaded', bootstrap);
-}
+bootstrap();
+document.addEventListener('readystatechange', bootstrap);
